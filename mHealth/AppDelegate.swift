2e09e40928c57ab21d78d6e768b03f3be2ed81b6
Loaner <loaner@estudiombp01.home>
2017-03-22 00:00:33 -0400

    THIS IS A SPAGHETTI COMMIT: BE WARNED!!!

--- mHealth/AppDelegate.swift
+++ mHealth/AppDelegate.swift
@@ -11,6 +11,9 @@ import Firebase
 import GoogleMaps
 import GooglePlaces
 
+/// u take my love for granted
+import CoreData
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,6 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        /*/ im gonna break stuff - travis
+       // let navigationController = window!.rootViewController as! UINavigationController
+        let controller = navigationController.topViewController as! HomeViewController
+        controller.managedObjectContext = managedObjectContext */
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyCRTOvuqLxcgIRuH1P02uvGG4HwCEDLEIU")
         GMSPlacesClient.provideAPIKey("AIzaSyCRTOvuqLxcgIRuH1P02uvGG4HwCEDLEIU")
@@ -46,7 +53,68 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    // MARK: - Core Data stack
+    
+    lazy var applicationDocumentsDirectory: URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.zedenem.MoonRunner" in the application's documents Application Support directory.
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls.last!
+    }()
 
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = Bundle.main.url(forResource: "mHealth", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.appendingPathComponent("mHealth.sqlite")
+        var error: NSError? = nil
+        var failureReason = "There was an error creating or loading the application's saved data."
+        do {
+            try coordinator!.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
+        }catch let error as NSError {
+            coordinator = nil
+            // Report any error we got.
+            var dict = [AnyHashable: Any]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSUnderlyingErrorKey] = error
+            
+            print("Error: \(error.domain)")
+            abort()
+        }
+        return coordinator
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext? = {
+        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+        let coordinator = self.persistentStoreCoordinator
+        if coordinator == nil {
+            return nil
+        }
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)//NSManagedObjectContext()
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        if let moc = self.managedObjectContext {
+            if moc.hasChanges{
+                do{
+                    try moc.save()
+                }catch let error as NSError{
+                    print("Error: \(error.domain)")
+                }
+            }
+        }
+    }
 
 }
 
