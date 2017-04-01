458d4bc05aa8d77b57d724a4ad18a68427a0994d
Loaner <loaner@estudiombp01.home>
2017-03-31 21:01:32 -0400

    fixed Facebook create account

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,29 +8,6 @@
 
 import Foundation
 
-enum CompassPoint: String {
-    case north:
-    case south
-    case east
-    case west
-}
+print("mHealth")
 
-
-let directionToHead: CompassPoint = .south
-
-
-
-
-switch directionToHead {
-case .north:
-    print("Lots of planets have a north")
-case .south:
-    print("Watch out for penguins")
-case .east:
-    print("Where the sun rises")
-case .west:
-    print("Where the skies are blue")
-}
-    
-    
    
